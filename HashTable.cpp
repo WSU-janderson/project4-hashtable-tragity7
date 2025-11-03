3b374467a7abb3c4c3b4d68411e6aeb6b1f47563
Sidra Ali <146482774+tragity7@users.noreply.github.com>
2025-11-03 13:12:41 -0500

    key-value pair implementation

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,3 +1,17 @@
 /**
  * HashTable.cpp
  */
+
+#include "HashTable.h"
+
+HashTableBucket::HashTableBucket() : key(""), value(0), type(ESS) {}
+
+HashTableBucket::HashTableBucket(const std::string& k, const size_t& v)
+    : key(k), value(v), type(NORMAL) {}
+
+void HashTableBucket::load(const std::string& k, const size_t& v) {
+    key = k;
+    value = v;
+    type = NORMAL;
+}
+
