192c591e667a9ccf6e6758a6db2085f798fa2497
Sidra Ali <146482774+tragity7@users.noreply.github.com>
2025-11-03 15:05:22 -0500

    semi complete hashtable implementation

--- HashTable.cpp
+++ HashTable.cpp
@@ -41,4 +41,36 @@ std::ostream& operator<<(std::ostream& os, const HashTableBucket& bucket) {
     return os;
 }
 
+HashTable::HashTable(size_t initCapacity)
+    : tableData(initCapacity), numElements(0) {
+    generateOffsets(initCapacity);
+}
+
+size_t HashTable::hashFunction(const std::string& key) const {
+    std::hash<std::string> hashFn;
+    return hashFn(key) % tableData.size();
+}
+
+void HashTable::generateOffsets(size_t capacity) {
+    offsets.clear();
+    for (size_t i = 1; i < capacity; ++i)
+        offsets.push_back(i);
+
+    // function to randomize
+}
+
+void HashTable::rehash() {
+    size_t newCapacity = tableData.size() * 2;
+    std::vector<HashTableBucket> oldData = tableData;
+
+    tableData = std::vector<HashTableBucket>(newCapacity);
+    numElements = 0;
+    generateOffsets(newCapacity);
+
+    for (const auto& bucket : oldData) {
+        if (bucket.getType() == NORMAL)
+            insert(bucket.getKey(), bucket.getValue());
+    }
+}
+
 
