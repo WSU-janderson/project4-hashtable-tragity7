4b004d922817cc48be7a8159dc6e03f68c5c4a8d
Sidra Ali <146482774+tragity7@users.noreply.github.com>
2025-11-03 13:20:08 -0500

    hashtable public class updated

--- HashTable.h
+++ HashTable.h
@@ -53,7 +53,18 @@ private:
     size_t probeIndex(const std::string& key, bool forInsert = false) const;
 
 public:
-// add public variables
+    HashTable(size_t initCapacity = 8);
+
+    bool insert(const std::string& key, const size_t& value);
+    bool remove(const std::string& key);
+    bool contains(const std::string& key) const;
+    std::optional<size_t> get(const std::string& key) const;
+    size_t& operator[](const std::string& key);
+
+    std::vector<std::string> keys() const;
+    double alpha() const;
+    size_t capacity() const;
+    size_t size() const;
 
     friend std::ostream& operator<<(std::ostream& os, const HashTable& ht);
 };
