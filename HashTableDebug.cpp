2aab47056daf5d04c045d23f0abe1e10bb23e15f
Sidra Ali <146482774+tragity7@users.noreply.github.com>
2025-11-03 14:20:53 -0500

    make hashtable test
    can't test write now since I havent't updated the insert function

--- HashTableDebug.cpp
+++ HashTableDebug.cpp
@@ -5,9 +5,22 @@
  */
 #include <iostream>
 
-// did not realize project 4 had been uploaded yet ;-;
-
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
+    HashTable fbnd(4);
+    std::cout << "Initial table (capacity = " << fbnd.capacity() << "):\n";
+    std::cout << fbnd << "\n";
+
+    // --- Insert Elements ---
+    std::cout << "Inserting elements...\n";
+    assert(fbnd.insert("apple", 10));
+    assert(fbnd.insert("banana", 20));
+    assert(fbnd.insert("orange", 30));
+    assert(fbnd.insert("lemon", 40));
+
+    std::cout << "\nTable after insertions:\n";
+    std::cout << fbnd;
+
+    
     return 0;
 }
